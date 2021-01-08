9b3c4d6eb9149021bfe648550ff66a9376cddcb2
Dan Geabunea <dangeabunea@hotmail.com>
2021-01-08 08:58:42 +0200

    Created before/after dor m1

--- 1-classes-and-objects/before/src/main/java/pluralsight/oop/Main.java
+++ 1-classes-and-objects/before/src/main/java/pluralsight/oop/Main.java
@@ -5,22 +5,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Partial flight plan
-        FlightPlan berlinToBucharest = new FlightPlan(
-                "BER",
-                "OTP"
-        );
-        System.out.println(berlinToBucharest);
 
-        System.out.println();
-
-        // Complete flight plan
-        FlightPlan parisToLondon = new FlightPlan(
-                "CDG",
-                "LHR",
-                LocalDateTime.of(2020, 01, 10, 23, 15),
-                List.of("CDG", "FARAR", "BAYKA", "DUDES", "LHR")
-        );
-        System.out.println(parisToLondon);
     }
 }
