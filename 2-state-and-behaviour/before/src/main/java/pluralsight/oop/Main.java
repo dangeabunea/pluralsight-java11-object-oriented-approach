9a145abc55673cb5ca586ac6694a6395a59efee5
Hazel <hazelchikara@hotmail.com>
2023-04-09 08:56:38 +0200

    OOP Methods & Variables

--- 2-state-and-behaviour/before/src/main/java/pluralsight/oop/Main.java
+++ 2-state-and-behaviour/before/src/main/java/pluralsight/oop/Main.java
@@ -2,11 +2,22 @@ package pluralsight.oop;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        /*
-        Create a radar target
-        Print the label
-        Send a change altitude command
-        Watch the behaviour
-         */
+            // Create radar target
+            RadarTarget os791 = new RadarTarget(
+                    "OS791",
+                    280,
+                    "B737-800");
+
+            // Print current label
+            System.out.println(os791.getLabel());
+            System.out.println();
+
+            // Change altitude command
+            os791.changeAltitude(200);
+            for (int i = 0; i < 5; i++) {
+                Thread.sleep(1000);
+                System.out.println(os791.getLabel());
+                System.out.println();
+            }
+        }
     }
-}
