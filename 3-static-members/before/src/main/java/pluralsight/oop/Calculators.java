9581439d95fb63b67502110c536301f5a12433af
Hazel <hazelchikara@hotmail.com>
2023-04-12 14:28:34 +0200

    Nested Classes eg

--- 3-static-members/before/src/main/java/pluralsight/oop/Calculators.java
+++ 3-static-members/before/src/main/java/pluralsight/oop/Calculators.java
@@ -1,5 +1,20 @@
 package pluralsight.oop;
+//Other that handle other
+public class  Calculators {
+//    This class is any other top level class though they are static, they uphold OOP by having
+//    The class keyword, arguments, constructors and another method that calculates
+    public static class Distance{
+        private int x1, x2, y1, y2;
 
-public class Calculators {
+        public Distance(int x1,int x2, int y1, int y2){
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+        public double calculate(){
+            return Math.sqrt((x2 -x1) * (x2 - x1) + (y2-y1) * (y2 -y1));
+        }
+    }
 
 }
--- 3-static-members/before/src/main/java/pluralsight/oop/Main.java
+++ 3-static-members/before/src/main/java/pluralsight/oop/Main.java
@@ -1,8 +1,23 @@
 package pluralsight.oop;
+//using the static import
+import static java.lang.System.out;
+import static pluralsight.oop.ConversionHelper.fromNmToMeters;
 
 public class Main {
     public static void main(String[] args) {
         int altitudeInFeet = 28000;
         double distInNm = 10;
+//        Call the class name and class methods to call the convertor methods
+        int altitudeInFL = ConversionHelper.fromFeetToFL(300);
+        System.out.println(altitudeInFeet + " fr -> FL " + altitudeInFL);
+
+//        Let's use a static import for the final conversion calling the method only
+         double distInMeters = fromNmToMeters(10);
+         out.println(distInNm + " NM ->  " + distInMeters + "m");
+
+//          To instantiate the distance class, we cannot use the distance class directly, we have to use
+//          calculate class then period and the static class name
+        Calculators.Distance d = new Calculators.Distance(1, 2 ,3,4 );
+        out.println("Distance is " + d.calculate());
     }
 }
